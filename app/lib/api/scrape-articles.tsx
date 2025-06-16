5be270351420521ebd07db7f44e3c3e929c07521
123Xylem <cucullen111@gmail.com>
2025-06-16 19:35:46 +0100

    scrape articles via seperate python backend

--- app/lib/api/scrape-articles.tsx
+++ app/lib/api/scrape-articles.tsx
@@ -1,14 +1,9 @@
-export default async function scrapeArticles(search: string = "") {
-  try {
-    console.log(search, "trying");
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/todos/${search}`
-    );
-    const data = await response.json();
+export default async function scrapeArticles(query: string) {
+  const queryString = query + " meta-analysis";
 
-    return data;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+  const res = await fetch(
+    `http://127.0.0.1:5000/api/pubmed?term=${queryString}&page=1`
+  );
+  const data = await res.json();
+  return data;
 }
